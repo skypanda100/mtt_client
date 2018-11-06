d330a475abf4db3b69165a2b4800a2a4541a8fbd
zhengdongtian <236520700@qq.com>
2018-11-06 13:38:45 +0800

    1. 食物评分重新布局

--- app/src/main/java/gzt/mtt/Adapter/FoodGradesAdapter.java
+++ app/src/main/java/gzt/mtt/Adapter/FoodGradesAdapter.java
@@ -16,9 +16,14 @@ import com.squareup.picasso.Picasso;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import cn.carbs.android.avatarimageview.library.AvatarImageView;
 import gzt.mtt.Constant;
 import gzt.mtt.R;
+import gzt.mtt.Util.TimeUtil;
 import me.zhanghai.android.materialratingbar.MaterialRatingBar;
 
 public class FoodGradesAdapter extends RecyclerView.Adapter {
@@ -76,24 +81,49 @@ public class FoodGradesAdapter extends RecyclerView.Adapter {
         try{
             JSONObject foodGrade = mFoodGrades.getJSONObject(i);
 
-            String imagePath = foodGrade.getString("imagePath");
+            List<String> images = new ArrayList<>();
+            images.add(foodGrade.getString("imagePath"));
             String alias = foodGrade.getString("alias");
             String avatar = foodGrade.getString("avatar");
             String dateTime = foodGrade.getString("dateTime");
+            Date date = TimeUtil.str2date(dateTime, "yyyy-MM-dd HH:mm");
+            List<String> dateChStrs = TimeUtil.date2chstr(date);
             String comment = foodGrade.getString("comment");
             int grade = foodGrade.getInt("grade");
+            if(foodGrade.has("others")) {
+                JSONArray others = foodGrade.getJSONArray("others");
+                for(int index = 0;index < others.length();index++) {
+                    images.add(others.getJSONObject(index).getString("imagePath"));
+                }
+            }
 
+            int width = 200;
+            int height = 200;
             if(this.mIsOneCol) {
-                Picasso.with(this.mContext).load(Constant.BaseImageUrl + avatar).into(foodGradesViewHolder.mAvatarAvatarImageView);
-                foodGradesViewHolder.mAliasTextView.setText(alias);
-                Picasso.with(this.mContext).load(Constant.BaseImageUrl + imagePath).into(foodGradesViewHolder.mFoodAppCompatImageView);
+//                Picasso.with(this.mContext).load(Constant.BaseImageUrl + avatar).into(foodGradesViewHolder.mAvatarAvatarImageView);
+//                foodGradesViewHolder.mAliasTextView.setText(alias);
+                for(int index = 0;index < 3 && index < images.size();index++) {
+                    String imagePath = images.get(index);
+                    switch (index) {
+                        case 0:
+                            Picasso.with(this.mContext).load(Constant.BaseImageUrl + imagePath).resize(width, height).centerCrop().into(foodGradesViewHolder.mFoodAppCompatImageView1);
+                            break;
+                        case 1:
+                            Picasso.with(this.mContext).load(Constant.BaseImageUrl + imagePath).resize(width, height).centerCrop().into(foodGradesViewHolder.mFoodAppCompatImageView2);
+                            break;
+                        case 2:
+                            Picasso.with(this.mContext).load(Constant.BaseImageUrl + imagePath).resize(width, height).centerCrop().into(foodGradesViewHolder.mFoodAppCompatImageView3);
+                            break;
+                    }
+                }
                 foodGradesViewHolder.mGradeMaterialRatingBar.setRating(grade);
-                foodGradesViewHolder.mDateTimeTextView.setText(dateTime);
+                foodGradesViewHolder.mDateTextView1.setText(dateTime.substring(8, 10));
+                foodGradesViewHolder.mDateTextView2.setText(dateChStrs.get(1) + "/" + dateChStrs.get(3));
                 foodGradesViewHolder.mCommentTextView.setText(comment);
+                foodGradesViewHolder.mTimeTextView.setText(dateTime.substring(11));
             } else {
-                int width = 200;
-                int height = 200;
-                Picasso.with(this.mContext).load(Constant.BaseImageUrl + imagePath).resize(width, height).centerCrop().into(foodGradesViewHolder.mFoodAppCompatImageView);
+                String imagePath = images.get(0);
+                Picasso.with(this.mContext).load(Constant.BaseImageUrl + imagePath).resize(width, height).centerCrop().into(foodGradesViewHolder.mFoodAppCompatImageView1);
             }
             }catch (Exception e) {
             e.printStackTrace();
@@ -111,23 +141,34 @@ public class FoodGradesAdapter extends RecyclerView.Adapter {
     class FoodGradesViewHolder extends RecyclerView.ViewHolder {
         AvatarImageView mAvatarAvatarImageView;
         TextView mAliasTextView;
-        AppCompatImageView mFoodAppCompatImageView;
+        AppCompatImageView mFoodAppCompatImageView1;
+        AppCompatImageView mFoodAppCompatImageView2;
+        AppCompatImageView mFoodAppCompatImageView3;
         MaterialRatingBar mGradeMaterialRatingBar;
-        TextView mDateTimeTextView;
+        TextView mDateTextView1;
+        TextView mDateTextView2;
         TextView mCommentTextView;
+        TextView mTimeTextView;
         public FoodGradesViewHolder(@NonNull View itemView) {
             super(itemView);
             if(mIsOneCol) {
                 this.mAvatarAvatarImageView = itemView.findViewById(R.id.avatar);
                 this.mAliasTextView = itemView.findViewById(R.id.alias);
-                this.mFoodAppCompatImageView = itemView.findViewById(R.id.food);
+                this.mFoodAppCompatImageView1 = itemView.findViewById(R.id.food1);
+                this.mFoodAppCompatImageView2 = itemView.findViewById(R.id.food2);
+                this.mFoodAppCompatImageView3 = itemView.findViewById(R.id.food3);
                 this.mGradeMaterialRatingBar = itemView.findViewById(R.id.grade);
-                this.mDateTimeTextView = itemView.findViewById(R.id.dateTime);
+                this.mDateTextView1 = itemView.findViewById(R.id.date1);
+                this.mDateTextView2 = itemView.findViewById(R.id.date2);
                 this.mCommentTextView = itemView.findViewById(R.id.comment);
+                this.mTimeTextView = itemView.findViewById(R.id.time);
+                this.mFoodAppCompatImageView1.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                this.mFoodAppCompatImageView2.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                this.mFoodAppCompatImageView3.setScaleType(ImageView.ScaleType.CENTER_CROP);
             } else {
-                this.mFoodAppCompatImageView = itemView.findViewById(R.id.food);
+                this.mFoodAppCompatImageView1 = itemView.findViewById(R.id.food);
+                this.mFoodAppCompatImageView1.setScaleType(ImageView.ScaleType.CENTER_CROP);
             }
-            this.mFoodAppCompatImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         }
     }
 }
--- app/src/main/java/gzt/mtt/Util/TimeUtil.java
+++ app/src/main/java/gzt/mtt/Util/TimeUtil.java
@@ -1,7 +1,10 @@
 package gzt.mtt.Util;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class TimeUtil {
     public static String date2str(Date date, String fmt) {
@@ -19,5 +22,55 @@ public class TimeUtil {
         }
 
         return date;
+    }
+
+    public static List<String> date2chstr(Date date) {
+        List<String> dateChStrs = new ArrayList<>();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        String yearStr = calendar.get(Calendar.YEAR) + "年";
+        dateChStrs.add(yearStr);
+        String monthStr = calendar.get(Calendar.MONTH) + 1 + "月";
+        dateChStrs.add(monthStr);
+        String dayStr = calendar.get(Calendar.DATE) + "日";
+        dateChStrs.add(dayStr);
+        int week = calendar.get(Calendar.DAY_OF_WEEK);
+        String weekStr = "";
+        switch (week) {
+            case 1:
+                weekStr = "周日";
+                break;
+            case 2:
+                weekStr = "周一";
+                break;
+            case 3:
+                weekStr = "周二";
+                break;
+            case 4:
+                weekStr = "周三";
+                break;
+            case 5:
+                weekStr = "周四";
+                break;
+            case 6:
+                weekStr = "周五";
+                break;
+            case 7:
+                weekStr = "周六";
+                break;
+            default:
+                break;
+        }
+        dateChStrs.add(weekStr);
+        String hourStr = calendar.get(Calendar.HOUR_OF_DAY) + "时";
+        dateChStrs.add(hourStr);
+        String minuteStr = calendar.get(Calendar.MINUTE) + "分";
+        dateChStrs.add(minuteStr);
+        String secondStr = calendar.get(Calendar.SECOND) + "秒";
+        dateChStrs.add(secondStr);
+
+        return dateChStrs;
     }
 }
