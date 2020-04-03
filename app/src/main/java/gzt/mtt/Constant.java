0380976d1eb9a6f45cb22937472cf6f6d3d64a1e
skypanda100 <236520700@qq.com>
2020-04-03 16:38:03 +0800

    1. constant

--- app/src/main/java/gzt/mtt/Constant.java
+++ app/src/main/java/gzt/mtt/Constant.java
@@ -3,6 +3,8 @@ package gzt.mtt;
 public class Constant {
     public static final String BaseUrl = "http://47.94.165.17:90/";
     public static final String BaseImageUrl = "http://47.94.165.17:84/";
+//    public static final String BaseUrl = "http://192.168.1.2:90/";
+//    public static final String BaseImageUrl = "http://192.168.1.2:12310/";
     public static final String ThumbnailSuffix = ".thumbnail";
 
 }
